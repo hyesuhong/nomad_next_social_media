ba4db045fa528aa15f3b900e7d4c3020366ad37b
Hyesu <honghs95@gmail.com>
2024-12-01 19:27:50 +0900

    feat: modify login form's action

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,48 +2,38 @@
 
 import { FormButton, FormInput } from '@/components/auth-form';
 import { CheckCircleIcon } from '@heroicons/react/24/outline';
-import Form from 'next/form';
-import { ChangeEvent, useActionState, useState } from 'react';
+import { FormEvent, startTransition, useActionState } from 'react';
 import { handleForm } from './action';
 
 export default function Login() {
 	const [state, action] = useActionState(handleForm, null);
-	const [formInput, setFormInput] = useState({
-		email: '',
-		username: '',
-		password: '',
-	});
 
-	const onInputChange = (ev: ChangeEvent<HTMLInputElement>) => {
-		const {
-			currentTarget: { name, value },
-		} = ev;
-		if (!(name in formInput)) {
-			return;
-		}
+	const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
+		ev.preventDefault();
 
-		setFormInput((prevInput) => ({ ...prevInput, [name]: value }));
+		const { currentTarget } = ev;
+		const formData = new FormData(currentTarget);
+
+		startTransition(() => {
+			action(formData);
+		});
 	};
 
 	return (
 		<main className='h-screen flex flex-col justify-center items-center gap-y-8 bg-indigo-50'>
 			<h2 className='text-xl font-bold'>Log in</h2>
-			<Form action={action} className='w-80 flex flex-col gap-y-6'>
+			<form onSubmit={handleSubmit} className='w-80 flex flex-col gap-y-6'>
 				<FormInput
 					label='Email'
 					placeholder='email'
 					name='email'
 					type='email'
-					value={formInput.email}
-					onChange={onInputChange}
 					errorMessage={state?.errors?.fieldErrors.email}
 				/>
 				<FormInput
 					label='Username'
 					placeholder='username'
 					name='username'
-					value={formInput.username}
-					onChange={onInputChange}
 					errorMessage={state?.errors?.fieldErrors.username}
 				/>
 				<FormInput
@@ -51,13 +41,11 @@ export default function Login() {
 					placeholder='password'
 					name='password'
 					type='password'
-					value={formInput.password}
-					onChange={onInputChange}
 					errorMessage={state?.errors?.fieldErrors.password}
 				/>
 
 				<FormButton>Log in</FormButton>
-			</Form>
+			</form>
 			{state?.status === 200 && (
 				<div className='flex items-center gap-x-2 w-80 p-4 rounded-md text-sm bg-emerald-500 text-zinc-50 animate-[fade-in_0.3s_ease-in-out]'>
 					<CheckCircleIcon className='w-6 h-6' />
