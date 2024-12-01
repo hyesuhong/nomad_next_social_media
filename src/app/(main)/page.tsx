72e59ca06ca23424601b61a7e93bd7608fb5d639
Hyesu <honghs95@gmail.com>
2024-12-02 02:03:06 +0900

    feat: rename profile route to share main layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,6 @@
-import { Header } from '@/components/common';
-
 export default function Home() {
 	return (
 		<>
-			<Header />
 			<main className='h-screen flex flex-col justify-center items-center gap-y-4 bg-indigo-50'>
 				<p>main</p>
 			</main>
--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,9 +1,6 @@
-import { Header } from '@/components/common';
-
 export default function Profile() {
 	return (
 		<>
-			<Header />
 			<main className='h-screen flex flex-col justify-center items-center gap-y-4 bg-indigo-50'>
 				<p>profile</p>
 			</main>
