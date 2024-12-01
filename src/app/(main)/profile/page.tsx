912cc5ebf8eeb90f9d7554fc2439c46ccfcf1181
Hyesu <honghs95@gmail.com>
2024-12-02 02:09:20 +0900

    feat: get logged in user data

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,8 +1,33 @@
-export default function Profile() {
+import db from '@/services/db';
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
+
+async function getLoggedInUser() {
+	const cookieStore = await cookies();
+	const session = await getIronSession<{ id?: string }>(cookieStore, {
+		cookieName: 'social_media_logged_in',
+		password: process.env.COOKIE_PASSWORD!,
+	});
+
+	if (!session.id) {
+		notFound();
+	}
+
+	const user = await db.user.findUnique({
+		where: { id: Number(session.id) },
+	});
+
+	return user || notFound();
+}
+
+export default async function Profile() {
+	const user = await getLoggedInUser();
+
 	return (
 		<>
 			<main className='h-screen flex flex-col justify-center items-center gap-y-4 bg-indigo-50'>
-				<p>profile</p>
+				<p>Welcome, {user.username}!</p>
 			</main>
 		</>
 	);
