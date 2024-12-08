b11c0279953f490d08e321e082c19018e70e7be4
Hyesu <honghs95@gmail.com>
2024-12-08 15:15:31 +0900

    feat: modify action for comment(default action -> optimistic update)

--- src/components/comment/comments.tsx
+++ src/components/comment/comments.tsx
@@ -1,5 +1,9 @@
+'use client';
+
+import { createComment } from '@/services/comment';
 import { Comment } from '@prisma/client';
 import Image from 'next/image';
+import { useOptimistic } from 'react';
 import sampleProfile from '../../../public/sample_profile.jpeg';
 import Form from './form';
 
@@ -13,13 +17,37 @@ interface CommentsProps {
 }
 
 export default function Comments({ postId, initialComments }: CommentsProps) {
+	const [optimisticComments, addOptimisticComment] = useOptimistic(
+		initialComments,
+		(prevComments, payload: CommentForList) => [payload, ...prevComments]
+	);
+
+	const commentAction = async (prevState: unknown, formData: FormData) => {
+		const commentContent = formData.get('content');
+		if (!commentContent) {
+			return;
+		}
+
+		const newComment: CommentForList = {
+			id: -1,
+			content: `${commentContent}`,
+			created_at: new Date(),
+			user: {
+				id: -1,
+				username: '...',
+			},
+		};
+		addOptimisticComment(newComment);
+		await createComment(null, formData);
+	};
+
 	return (
 		<>
-			<Form postId={postId} />
+			<Form postId={postId} commentAction={commentAction} />
 
-			{initialComments.length > 0 && (
+			{optimisticComments.length > 0 && (
 				<div className='border border-zinc-300'>
-					{initialComments.map((comment) => (
+					{optimisticComments.map((comment) => (
 						<dl className='flex gap-x-4 p-2' key={comment.id}>
 							<dt>
 								<div className='w-12 h-12 overflow-hidden rounded-full '>
--- src/components/comment/form.tsx
+++ src/components/comment/form.tsx
@@ -1,29 +1,17 @@
-'use client';
-
-import { createComment } from '@/services/comment';
-import { FormEvent, startTransition, useActionState } from 'react';
+import { useActionState } from 'react';
 import SubmitButton from './submit-button';
 
 interface FormProps {
 	postId: number;
+	commentAction: (prevState: unknown, formData: FormData) => Promise<void>;
 }
 
-export default function Form({ postId }: FormProps) {
-	const [state, action] = useActionState(createComment, null);
+export default function Form({ postId, commentAction }: FormProps) {
+	const [, action] = useActionState(commentAction, null);
 
-	const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
-		ev.preventDefault();
-
-		const { currentTarget } = ev;
-		const formData = new FormData(currentTarget);
-
-		startTransition(() => {
-			action(formData);
-		});
-	};
 	return (
 		<form
-			onSubmit={handleSubmit}
+			action={action}
 			className='flex flex-col items-end gap-y-2 border border-zinc-300 p-2'
 		>
 			<textarea
@@ -31,16 +19,7 @@ export default function Form({ postId }: FormProps) {
 				className='w-full h-24 bg-transparent border border-zinc-300 outline-none text-sm p-2 resize-none overflow-x-hidden overflow-y-auto focus:border-indigo-400'
 			></textarea>
 			<input type='hidden' name='post_id' value={postId} />
-			<div className='flex justify-end items-center gap-x-2'>
-				{state?.errors && 'content' in state.errors && (
-					<p className='text-xs text-red-500'>
-						{state.errors.content?.map((err, index) => (
-							<span key={index}>{err}</span>
-						))}
-					</p>
-				)}
-				<SubmitButton />
-			</div>
+			<SubmitButton />
 		</form>
 	);
 }
