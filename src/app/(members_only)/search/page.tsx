c2f283dd25906b773f4b4c422aea6ae6a0d2fff6
Hyesu <honghs95@gmail.com>
2024-12-09 13:04:46 +0900

    feat: create new page(search)

---
+++ src/app/(members_only)/search/page.tsx
@@ -0,0 +1,15 @@
+export default function Search() {
+	return (
+		<main>
+			<section className='max-w-xl w-full mx-auto px-4 py-8'>
+				<form action=''>
+					<input type='text' />
+					<button>Search</button>
+				</form>
+			</section>
+			<section className='max-w-xl w-full mx-auto px-4 py-8'>
+				<p>Search first</p>
+			</section>
+		</main>
+	);
+}
