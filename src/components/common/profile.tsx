59075df9d2a4d42f35c3f38409194e59ff70b4bb
Hyesu <honghs95@gmail.com>
2024-12-10 14:46:51 +0900

    feat: create profile component

---
+++ src/components/common/profile.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { IcoUser } from '@/assets/icons';
+import Image, { ImageLoader } from 'next/image';
+import { SyntheticEvent, useState } from 'react';
+
+const initialSize = {
+	width: 0,
+	height: 0,
+};
+
+interface ProfileProps {
+	name: string;
+	imageUrl?: string;
+	size?: 'small' | 'medium' | 'large';
+}
+
+export default function Profile({
+	name,
+	imageUrl,
+	size = 'small',
+}: ProfileProps) {
+	const [imageSize, setImageSize] = useState(initialSize);
+	const imageLoader: ImageLoader = ({ src, width, quality }) => {
+		return `${src}?w=${width}&q=${quality || 80}`;
+	};
+
+	const onLoad = (ev: SyntheticEvent<HTMLImageElement>) => {
+		const {
+			currentTarget: { naturalHeight, naturalWidth },
+		} = ev;
+
+		setImageSize({ width: naturalWidth, height: naturalHeight });
+	};
+
+	return (
+		<>
+			<div
+				className={`${size === 'small' ? 'size-12' : size === 'medium' ? 'size-14' : 'size-20'} border border-zinc-500 rounded-full overflow-hidden [&_svg]:size-full [&_svg]:translate-y-[10%]`}
+			>
+				{imageUrl ? (
+					<Image
+						src={imageUrl}
+						alt={name}
+						width={imageSize.width}
+						height={imageSize.height}
+						loader={imageLoader}
+						onLoad={onLoad}
+						loading='eager'
+					/>
+				) : (
+					<IcoUser />
+				)}
+			</div>
+		</>
+	);
+}
