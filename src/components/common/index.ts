4c5afa512e6032681e4e7c938b3b39f262601c9a
Hyesu <honghs95@gmail.com>
2024-12-15 22:49:57 +0900

    remove: delete header component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,5 @@
 import Button from './button';
-import Header from './header';
 import Profile from './profile';
 import TabBar from './tab-bar';
 
-export { Button, Header, Profile, TabBar };
+export { Button, Profile, TabBar };
